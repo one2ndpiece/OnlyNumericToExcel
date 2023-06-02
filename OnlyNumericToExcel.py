6489513e13eff0a206366f9b7dfb7231891d1f4a
YutoTonami <toranpu.card.yagiri@gmail.com>
2023-06-02 19:17:00 +0900

    add streamlit

--- OnlyNumericToExcel.py
+++ OnlyNumericToExcel.py
@@ -1,25 +1,38 @@
-import re
+import streamlit as st
 import pandas as pd
+import re
+import os
 
-def extract_numbers(file_name):
+def extract_numbers(file):
     data = []
 
-    with open("sample2.txt", 'r', encoding='utf-8', errors='ignore') as file:
-        for line in file:
-            # Finding all numbers including negative numbers
-            numbers = re.findall(r'-?\d+\.?\d*', line)
-            # Converting all found strings to floats or integers
-            numbers = [float(num) if '.' in num else int(num) for num in numbers]
-            data.append(numbers)
+    for line in file:
+        # Converting bytes to string
+        line = line.decode('utf-8')
+        # Finding all numbers including negative numbers
+        numbers = re.findall(r'-?\d+\.?\d*', line)
+        # Converting all found strings to floats or integers
+        numbers = [float(num) if '.' in num else int(num) for num in numbers]
+        data.append(numbers)
 
     return data
 
-# Replace with your text file name
-file_name = 'your_text_file.txt'
-data = extract_numbers(file_name)
+# Streamlit code
+st.title('Number Extractor')
 
-# Creating a pandas DataFrame from the list of lists
-df = pd.DataFrame(data)
+uploaded_file = st.file_uploader("Choose a TXT file", type="txt")
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+    st.write(file_details)
 
-# Writing the DataFrame to an Excel file
-df.to_excel('output2.xlsx', index=False, header=False)
+    data = extract_numbers(uploaded_file)
+    df = pd.DataFrame(data)
+
+    st.dataframe(df)
+
+    if st.button('Save to Excel'):
+        output_path = st.text_input('Enter output path', '')
+        excel_file = st.text_input('Enter Excel file name', 'output.xlsx')
+        full_path = os.path.join(output_path, excel_file)
+        df.to_excel(full_path, index=False, header=False)
+        st.success(f'Excel file saved as {full_path}')
